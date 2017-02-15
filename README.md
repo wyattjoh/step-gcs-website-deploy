29bb8c84dff3ebeb267aabbf5b9a68bf70ae8861
Wyatt Johnson <wyattjoh@gmail.com>
2017-02-15 10:57:28 -0700

    Removed requirement of .boto file in repo

--- README.md
+++ README.md
@@ -2,16 +2,6 @@
 
 A step that deploys website to Google Cloud Storage.
 
-It requires a `.boto` config file on the root of the repository,
-and requires `gs_oauth2_refresh_token` and `default_project_id` lines in `.boto` like below:
-
-    [Credentials]
-    gs_oauth2_refresh_token =
-    [GSUtil]
-    default_project_id =
-
-see: https://cloud.google.com/storage/docs/gsutil/commands/config
-
 It applies gzip content-encoding to file uploads with below extensions.
 
   * css
@@ -35,32 +25,16 @@ It applies gzip content-encoding to file uploads with below extensions.
 
 * `dir` - A path to directory that is root of the website. The default value is `public`.
 
-### initialize options
-
-* `initialize` - A flag for initialize a bucket of the Google Cloud Storage via `gsutil mb` command. Not empty is True.
-* `class` - A bucket storage class of the Google Cloud Storage.
-* `location` - A bucket location of the Google Cloud Storage.
-
-see: https://cloud.google.com/storage/docs/gsutil/commands/mb#bucket-locations
-
-It will be created with the default (Standard) storage class.
-It will be set the MainPageSuffix property to `index.html` and the NotFoundPage property to `404.html`.
-
 ## Example
 
 ```
 deploy:
   steps:
-  - michilu/gcs-website-deploy:
+  - wyattjoh/gcs-website-deploy:
     bucket:   example.com
     project:  $GOOGLE_PROJECT_ID
     token:    $GOOGLE_REFRESH_TOKEN
 
     # options
     dir:      public
-
-    # if you want to initialize a bucket
-    initialize: not_empty
-    class:      DURABLE_REDUCED_AVAILABILITY
-    location:   ASIA-EAST1
 ```
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 function install_gcs() {
-debug 'Installing gsutil'
-curl -sO https://storage.googleapis.com/pub/gsutil.tar.gz
-rm -rf $WERCKER_CACHE_DIR/gsutil
-tar xfz gsutil.tar.gz -C $WERCKER_CACHE_DIR
+  debug 'Installing gsutil'
+  curl -sO https://storage.googleapis.com/pub/gsutil.tar.gz
+  rm -rf $WERCKER_CACHE_DIR/gsutil
+  tar xfz gsutil.tar.gz -C $WERCKER_CACHE_DIR
 }
 
 export PATH=${PATH}:$WERCKER_CACHE_DIR/gsutil
@@ -15,23 +15,20 @@ fi
 
 debug 'setting gsutil'
 
-sed -i "/^\[Credentials\]/,/^gs_oauth2_refresh_token/ s@^\(gs_oauth2_refresh_token =\).*@\1 $WERCKER_GCS_WEBSITE_DEPLOY_TOKEN@" .boto
-sed -i "/^\[GSUtil\]/,/^default_project_id/ s@^\(default_project_id =\).*@\1 $WERCKER_GCS_WEBSITE_DEPLOY_PROJECT@" .boto
-export BOTO_PATH=$PWD/.boto
+cat > .boto <<EOF
+[Credentials]
+gs_oauth2_refresh_token = ${WERCKER_GCS_WEBSITE_DEPLOY_TOKEN}
 
-# if WERCKER_GCS_WEBSITE_DEPLOY_INITIALIZE is not empty
-if [ -n "$WERCKER_GCS_WEBSITE_DEPLOY_INITIALIZE" ]; then
-debug 'Initial setting bucket'
-gsutil mb -c $WERCKER_GCS_WEBSITE_DEPLOY_CLASS -l $WERCKER_GCS_WEBSITE_DEPLOY_LOCATION gs://$WERCKER_GCS_WEBSITE_DEPLOY_BUCKET
-gsutil web set -m index.html -e 404.html gs://$WERCKER_GCS_WEBSITE_DEPLOY_BUCKET
-gsutil defacl ch -u AllUsers:R gs://$WERCKER_GCS_WEBSITE_DEPLOY_BUCKET
-fi
+[GSUtil]
+default_project_id = ${$WERCKER_GCS_WEBSITE_DEPLOY_PROJECT}
+EOF
+
+export BOTO_PATH=$PWD/.boto
 
 debug 'Starting deployment'
 
 # if WERCKER_GCS_WEBSITE_DEPLOY_DIR is empty
-[ -z "$WERCKER_GCS_WEBSITE_DEPLOY_DIR" ] &&
-  WERCKER_GCS_WEBSITE_DEPLOY_DIR=public
+[ -z "$WERCKER_GCS_WEBSITE_DEPLOY_DIR" ] && WERCKER_GCS_WEBSITE_DEPLOY_DIR=public
 
 gsutil -m rsync -r -d $WERCKER_GCS_WEBSITE_DEPLOY_DIR gs://$WERCKER_GCS_WEBSITE_DEPLOY_BUCKET
 gsutil -m cp -r -z html,css,js,xml,txt,json,map,svg $WERCKER_GCS_WEBSITE_DEPLOY_DIR/* gs://$WERCKER_GCS_WEBSITE_DEPLOY_BUCKET
